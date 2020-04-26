ba16f413d54b67c794f528fe872da429955c965e
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-04-26 06:19:04 -0400

    Clean code user route

--- routes/user-route/user.js
+++ routes/user-route/user.js
@@ -1,27 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
 const routeGuard = require('../../configs/route-guard.config');
 
 const User = require('../../models/User.model');
 
-/* User profile page */
-router.get('/api/profile', routeGuard, (req, res) => {
-  User.findOne({_id: req.user._id})
+// User profile page 
+router.get('/api/profile', routeGuard, (req, res) => { console.log(`BE user.js the req.user is................................... ${req.user}`)
+  User.findById({_id: req.user._id})
   .then(currentUser => {
-    console.log(`BE user.js the current user is ${currentUser}`)
     res.status(200).json({user: currentUser});
   })
   .catch(err => next(err))
-
+  
 });
 
-
+// Update profile page
 router.post('/api/profile/upload', routeGuard, (req, res, next) => {
-  console.log(">>>>>>>>>>>>>>>>>> updating profile <<<<<<<<<<<<<<<<<<<<< ", req.user);
-  // const userInputInfo = req.body;
-  
-  // Prevent user to force empty input
   const {username, email} = req.body
   if (username === "" || email === "") {
     res.status(404).json({
@@ -29,7 +23,7 @@ router.post('/api/profile/upload', routeGuard, (req, res, next) => {
     });
     return;
   }
-  //Instead of req.params i changed to user._id due to 
+  //Instead of req.params it changed to user._id ***
   User.findByIdAndUpdate( req.user._id, req.body, {new:true})
   .then( updatedUser => {
     res.status(200).json({user: updatedUser});
