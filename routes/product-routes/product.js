d2070226634cebbc440d1d50f37afd75b8a63816
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-04-26 06:21:39 -0400

    Delete console.log

--- routes/product-routes/product.js
+++ routes/product-routes/product.js
@@ -10,7 +10,6 @@ const Product = require("../../models/Product.model");
     Product.find()
         .then((productsFromDB) => {
             res.status(200).json(productsFromDB);
-            console.log(`BE product.js LINE 13 ++++++++++++++++++++++++++++++++`, productsFromDB[0])
         })
         .catch((err) => console.log(err));
 });
@@ -26,36 +25,5 @@ router.get("/products/:productId", (req, res, next) => {
       })
       .catch((err) => console.log(err));
 });
-
-// Add product
-
-router.post("/products/:productId/add", (req, res) => {
-    Product.findByIdAndUpdate(req.params.productId)
-    console.log(`BE product.js LINE 23 =====`, req.params.productId)
-        const { color, size, _id } = req.body;
-        console.log('BE product.js LINE34 this is id of product', _id)
-      
-        if (!size || !quantity || !color) {
-          res.status(401).json({
-            message: 'Please select size, color and quantity.'
-          });
-          return;
-        }
-      
-       
-})
-
-// Delete Product
-router.delete("/details/:productId", (req, res, next) => {
-  Product.findByIdAndDelete(req.params.productId)
-      .then(() => {
-          res.status(200).json({
-              message: "Successful Deletion of Product",
-          });
-      })
-      .catch((err) => console.log(err));
-});
-
-
 
 module.exports = router;
